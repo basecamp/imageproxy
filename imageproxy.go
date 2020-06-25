be73dfa4ff1ff0292661b5df9cc2a187cd7956e5
Blake Stoddard <blake.m.stoddard@gmail.com>
2020-06-25 10:15:50 -0400

    Send an Accept-Lanaguage header to fix https://3.basecamp.com/2914079/buckets/15908060/todos/2791881636

--- imageproxy.go
+++ imageproxy.go
@@ -181,6 +181,7 @@ func (p *Proxy) serveImage(w http.ResponseWriter, r *http.Request) {
 
 	actualReq, _ := http.NewRequest("GET", req.String(), nil)
 	actualReq.Header.Set("Accept", "*/*")
+	actualReq.Header.Set("Accept-Langauge", "*")
 	if p.UserAgent != "" {
 		actualReq.Header.Set("User-Agent", p.UserAgent)
 	}
